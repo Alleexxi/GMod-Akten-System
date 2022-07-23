e40c9239015cc2f015b9b126bbc525a0e384b209
Alleexxi <66967403+Alleexxi@users.noreply.github.com>
2022-07-23 12:32:07 +0200

    Update akten-server.lua

--- Akten/lua/autorun/akten-server.lua
+++ Akten/lua/autorun/akten-server.lua
@@ -6,19 +6,27 @@ Alleexxii.Akten = Alleexxii.Akten or {}
 util.AddNetworkString("Alleexxii_Akten_Request")
 util.AddNetworkString("Alleexxii_Akten_Send")
 
-sql.Query( "CREATE TABLE IF NOT EXISTS Akten_data_jedi ( SteamID TEXT,JobId INTEGER, Name TEXT, Rang TEXT, Meister TEXT, Straftat TEXT, Datum TEXT, Haftzeit TEXT )" )
-sql.Query( "CREATE TABLE IF NOT EXISTS Akten_data_sith ( SteamID TEXT,JobId INTEGER, Name TEXT, Rang TEXT, Meister TEXT, Straftat TEXT, Datum TEXT, Haftzeit TEXT )" )
+--sql.Query( "CREATE TABLE IF NOT EXISTS Akten_data_jedi ( SteamID TEXT,JobId INTEGER, Name TEXT, Rang TEXT, Meister TEXT, Straftat TEXT, Datum TEXT, Haftzeit TEXT )" )
+--sql.Query( "CREATE TABLE IF NOT EXISTS Akten_data_sith ( SteamID TEXT,JobId INTEGER, Name TEXT, Rang TEXT, Meister TEXT, Straftat TEXT, Datum TEXT, Haftzeit TEXT )" )
+
+if not file.IsDir("akten", "DATA") then
+    file.CreateDir("akten")
+end
 
 
-
+TEAM_JEDI = 1
+TEAM_SITH = 0
 Alleexxii.Akten.JediOrSith = {
-    -- Musst halt hier noch die Team IDs angeben usw.
-    -- [Team] = {NameDesMySqlTables, DürfenSieEsBenutzen}
-    -- Zum beispiel:
     [TEAM_JEDI] = {"Akten_data_jedi",true},
     
     [TEAM_SITH] = {"Akten_data_sith",false},
 }
+
+for i,v in pairs(Alleexxii.Akten.JediOrSith) do
+    if not file.Exists("akten/"..v[1]..".txt", "DATA") then
+        file.Write("akten/"..v[1]..".txt",util.TableToJSON({}))
+    end
+end
 
 function GetJediOrSith(ply)
     return Alleexxii.Akten.JediOrSith[ply:getJobTable().team]
@@ -28,11 +36,31 @@ function SendAkten(ply,target)
     local JediOrSith = GetJediOrSith(ply)
     if not JediOrSith then return end
     if not JediOrSith[2] then return end
-    local Akten
-    if not target then Akten = sql.Query("SELECT * FROM " .. JediOrSith[1]) else Akten = sql.Query( "SELECT * FROM "..JediOrSith[1].." WHERE SteamID = ".. sql.SQLStr(target) ) end
 
+    if target then
+        local normaltable = util.JSONToTable(file.Read("akten/"..JediOrSith[1]..".txt"));
+        local targettable = {};
+        for i,v in pairs(normaltable) do
+            if v.SteamID == target then
+                targettable[#targettable + 1] = v
+            end
+        end
+
+        local compress = util.Compress(util.TableToJSON(targettable))
+        local compressbytes = #compress
+        net.Start("Alleexxii_Akten_Request")
+            net.WriteUInt(compressbytes,16)
+            net.WriteData(compress,compressbytes)
+        net.Send(ply)
+
+        return
+    end
+
+    local compress = util.Compress(file.Read("akten/"..JediOrSith[1]..".txt"))
+    local compressbytes = #compress
     net.Start("Alleexxii_Akten_Request")
-        net.WriteTable(Akten or {})
+        net.WriteUInt(compressbytes,16)
+        net.WriteData(compress,compressbytes)
     net.Send(ply)
 end
 
@@ -41,26 +69,33 @@ function Akten_Send(ply)
     net.Send(ply)
 end
 
+local meta = FindMetaTable("Player")
+function meta:GetJobRankName()
+    return "-"
+end
+
 net.Receive("Alleexxii_Akten_Send",function(len,ply)
     local Ent,Meister,Straftat,Minuten = net.ReadEntity(),net.ReadString(),net.ReadString(),net.ReadString()
     if not IsValid(Ent) then return end
-    if not Ent:IsPlayer() then return end
     if not Meister or not Straftat or not tonumber(Minuten) then return end
     local JediOrSith = GetJediOrSith(ply)
     if not JediOrSith then return end
     if not JediOrSith[2] then return end
-    sql.Query( string.format("INSERT INTO " .. JediOrSith[1] .. " ( SteamID,JobId,Name,Rang,Meister,Straftat,Datum,Haftzeit ) VALUES( %s,%d,%s,%s,%s,%s,%s,%s )",sql.SQLStr(Ent:SteamID()),Ent:getJobTable().team,sql.SQLStr(Ent:getDarkRPVar("rpname")),sql.SQLStr(Ent:GetJobRankName() or "-"),sql.SQLStr(Meister),sql.SQLStr(Straftat),sql.SQLStr(os.date('%Y-%m-%d %H:%M:%S')),sql.SQLStr(Minuten)) )
-end)
 
-
-concommand.Add("DropJediAkten",function()
-    sql.Query("DROP TABLE Akten_data_jedi")
-    sql.Query( "CREATE TABLE IF NOT EXISTS Akten_data_jedi ( SteamID TEXT,JobId INTEGER, Name TEXT, Rang TEXT, Meister TEXT, Straftat TEXT, Datum TEXT, Haftzeit TEXT )" )
-end)
-
-concommand.Add("DropSithAkten",function()
-    sql.Query("DROP TABLE Akten_data_sith")
-    sql.Query( "CREATE TABLE IF NOT EXISTS Akten_data_sith ( SteamID TEXT,JobId INTEGER, Name TEXT, Rang TEXT, Meister TEXT, Straftat TEXT, Datum TEXT, Haftzeit TEXT )" )
+    local Table = {
+        ["SteamID"] = Ent:SteamID(),
+        ["JobId"] = Ent:getJobTable().team,
+        ["Name"] = Ent:getDarkRPVar("rpname"),
+        ["Rang"] = Ent:GetJobRankName(),
+        ["Meister"] = Meister,
+        ["Straftat"] = Straftat,
+        ["Datum"] = os.date('%Y-%m-%d %H:%M:%S'),
+        ["Haftzeit"] = Minuten,
+    }
+    
+    local Data = util.JSONToTable(file.Read("akten/"..JediOrSith[1]..".txt"))
+    Data[#Data+1] = Table
+    file.Write("akten/"..JediOrSith[1]..".txt",util.TableToJSON(Data))
 end)
 
 hook.Add( "PlayerSay", "Akten_Commands", function( ply, text )
